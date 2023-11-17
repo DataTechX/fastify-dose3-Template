11e6b4de910208eb0042c99f332a71ca182712ad
JKTheRipperTH <69198341+JKTheRipperTH@users.noreply.github.com>
2023-11-17 18:48:49 +0700

    Delete fastify-dose3 directory

--- fastify-dose3/routes/api/root.js
+++
@@ -1,5 +0,0 @@
-const root = async (request, reply) => { // async function
-    return ({ hello: 'world' }) // return json
-} // end async function
-
-module.exports = root // export root
--- fastify-dose3/routes/router.js
+++
@@ -1,7 +0,0 @@
-
-const root = require('./api/root') // import root.js
-
-module.exports = async function (fastify, opts,next) { // export async function
-    fastify.get('/', root) // GET / -> root.js
-    next() // next
-}
